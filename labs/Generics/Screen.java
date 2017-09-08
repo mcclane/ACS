7159351670c1798d823bd5b5e669242996c4ff54
McClane Howland <mcclane.howland@gmail.com>
2017-09-08 14:23:43 -0700

    finished challenge

--- labs/Generics/Screen.java
+++ labs/Generics/Screen.java
@@ -14,12 +14,16 @@ public class Screen extends JPanel implements ActionListener {
     private JTextField search;
 	private JButton select;
 	private JButton clear;
+    private JButton addStudent;
 
     private JTextField addPeriod;
     private JTextField addClass;
     private JButton addClassPeriod;
 
-    private boolean selected;
+    private JTextField studentName;
+    private JButton submitNewStudent;
+
+    private int selection = 0;
 
     ArrayList<Pair<Student, Schedule>> sl;
     Pair<Student, Schedule> selectedStudent;
@@ -36,9 +40,9 @@ public class Screen extends JPanel implements ActionListener {
         s.addClass(3, "US History");
         sl.add(new Pair<Student, Schedule>(new Student("Jose", "Jose.jpg"), s));
         s = new Schedule();
-        s.addClass(1, "Algebra");
-        s.addClass(2, "Civics");
-        s.addClass(3, "Stats");
+        s.addClass(1, "Space Camp");
+        s.addClass(2, "Chinese Studies");
+        s.addClass(3, "Advanced Computer Science");
         sl.add(new Pair<Student, Schedule>(new Student("Austin", "Austin.jpg"), s));
         s = new Schedule();
         s.addClass(1, "Multivariable Calculus");
@@ -63,6 +67,18 @@ public class Screen extends JPanel implements ActionListener {
 		this.add(addClassPeriod); //add to JPanel
         addClassPeriod.setVisible(false);
 
+		addStudent = new JButton("Add Student");
+		addStudent.setBounds(450, 50, 200, 30); //sets the location and size
+	    addStudent.addActionListener(this); //add the listener
+		this.add(addStudent); //add to JPanel
+        addStudent.setVisible(true);
+
+		submitNewStudent = new JButton("Submit Student");
+		submitNewStudent.setBounds(250, 450, 300, 30); //sets the location and size
+	    submitNewStudent.addActionListener(this); //add the listener
+		this.add(submitNewStudent); //add to JPanel
+        submitNewStudent.setVisible(false);
+
 		//TextField
 		search = new JTextField(20);
 		search.setBounds(50,50, 80, 30);
@@ -72,11 +88,17 @@ public class Screen extends JPanel implements ActionListener {
 		addPeriod.setBounds(450,250, 50, 30);
 		this.add(addPeriod);
         addPeriod.setVisible(false);
-        
+       
         addClass = new JTextField(20);
 		addClass.setBounds(510, 250, 200, 30);
 		this.add(addClass);
         addClass.setVisible(false);
+
+        studentName = new JTextField(20);
+		studentName.setBounds(250,350, 50, 30);
+		this.add(studentName);
+        studentName.setVisible(false);
+ 
     }
     public Dimension getPreferredSize() {
         //Sets the size of the panel
@@ -93,7 +115,10 @@ public class Screen extends JPanel implements ActionListener {
 
         int x = 150;
         int y = 200;
-        if(selected) {
+        if(selection == 1) {
+            studentName.setVisible(false);
+            submitNewStudent.setVisible(false);
+ 
             //draw the student
             selectedStudent.getKey().drawStudent(g, x, y);
             String lines[] = selectedStudent.getValue().toString().split("\\n");
@@ -101,12 +126,19 @@ public class Screen extends JPanel implements ActionListener {
                 g.drawString(l, x+110, y+30);
                 y += 20;
             }
+
             //set the textfields and jbuttons to visible
             addClass.setVisible(true);
             addPeriod.setVisible(true);
             addClassPeriod.setVisible(true);
         }
+        else if(selection == 2) {
+            studentName.setVisible(true);
+            submitNewStudent.setVisible(true);
+        }
         else {
+            studentName.setVisible(false);
+            submitNewStudent.setVisible(false);
             addClass.setVisible(false);
             addPeriod.setVisible(false);
             addClassPeriod.setVisible(false);
@@ -122,18 +154,24 @@ public class Screen extends JPanel implements ActionListener {
             for(Pair<Student, Schedule> ss: sl) {
                 if(ss.getKey().toString().equals(text)) {
                     selectedStudent = ss;
-                    selected = true;
+                    selection = 1;
                     System.out.println("selected");
                     break;
                 }
             }
         }
         else if(e.getSource() == clear) {
-            selected = false;
+            selection = 0;
             System.out.println("cleared");
         }
         else if(e.getSource() == addClassPeriod) {
             selectedStudent.getValue().addClass(Integer.parseInt(addPeriod.getText()), addClass.getText());
+        }
+        else if(e.getSource() == addStudent) {
+            selection = 2;
+        }
+        else if(e.getSource() == submitNewStudent) {
+            sl.add(new Pair<Student, Schedule>(new Student(studentName.getText(), "Jennifer.jpg"), new Schedule()));
         }
         repaint();
     }
