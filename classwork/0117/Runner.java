cc0405bb3e10461a81408759142d3c61e18250aa
McClane Howland <mcclane.howland@gmail.com>
2018-01-22 13:55:53 -0800

    asdf

--- classwork/0117/Runner.java
+++ classwork/0117/Runner.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 class Song {
   private String artist;
   private String name;
-  public Song(String a,String b)
+  private int time;
+  public Song(String a,String b, int time)
   {
     artist=a;
     name=b;
+    this.time = time;
   }
-  
   public boolean equals(Object o)
   {
     Song temp=(Song)o;
@@ -18,7 +19,6 @@ class Song {
     }
     return(false);
   }
-  
   public String toString()
   {
     return(artist+": "+name);
@@ -28,6 +28,9 @@ class Song {
   }
   public String name() {
       return name;
+  }
+  public int time() {
+      return time;
   }
 }
 class Node<E> {
@@ -147,28 +150,66 @@ class DLList<E> {
         if(size == 0) {
             return "[]";
         }
-        String out = "[";
+        String out = "";
         Node<E> current = dummy.next();
+        int time = 1;
         while(current != dummy) {
-            out += current.get()+", ";
+            out += time+". "+current.get()+"\n ";
             current = current.next();
         }
-        out = out.substring(0, out.length()-2) + "]";
         return out;
     }
+    public void print() {
+        _print(dummy.next());
+    }
+    public void _print(Node<E> n) {
+        if(n.get() == null) 
+            return;
+        else 
+            System.out.println(n.get());
+            _print(n.next());
+    }
+    public void reverse() {
+        for(Node<E> current = dummy.next();current.get() != null;current = current.prev()) {
+            Node<E> prev = current.prev();
+            current.setPrev(current.next());
+            current.setNext(prev);
+        }
+        Node<E> prev = dummy.prev();
+        dummy.setPrev(dummy.next());
+        dummy.setNext(prev);
+    }
 }
-
 public class Runner {
+    public static void main(String[] args) {
+        DLList<String> dl = new DLList<String>();
+        dl.add("Bear");
+        dl.add("Bat");
+        dl.add("Dog");
+        dl.add("Horse");
+        dl.add("Rabbit");
+        System.out.println("Recursive Print");
+        dl.print();
+        System.out.println();
+        System.out.println("Reverse Order");
+        dl.print();
+        dl.reverse();
+        dl.print();
+    }
+}
+/*public class Runner {
 
+    static int time = 0;
     static DLList<Song> original = new DLList<Song>();
     static DLList<Song> sorted = new DLList<Song>();
 
     public static void main(String[] args) {
-        original.add(new Song("A", "Ant"));
-        original.add(new Song("A", "Anteater"));
-        original.add(new Song("B", "Bear"));
-        original.add(new Song("C", "Cat"));
-        original.add(new Song("D", "Duck"));
+        original.add(new Song("A", "Ant", 0));
+        original.add(new Song("A", "Anteater", 1));
+        original.add(new Song("B", "Bear", 2));
+        original.add(new Song("C", "Cat", 3));
+        original.add(new Song("D", "Duck", 4));
+        time = 5;
         sorted = original;
         Scanner sc = new Scanner(System.in);
         String name;
@@ -177,7 +218,6 @@ public class Runner {
         String sortby = "time";
         while(true) {
             System.out.println("1. Add a song\t\t\t2. Display Song List\n3. Delete by name and artist\t4. Delete by index\n5. Delete by artist\t\t6. Delete by name\n7. Sort by artist\t\t8. Sort by name\n9. Sort by time\t\t\t10. Search by artist\n11. Quit");
-            System.out.println(sorted);
             int selection = sc.nextInt();
             switch(selection) {
                 case 1:
@@ -185,8 +225,8 @@ public class Runner {
                     name = sc.next();
                     System.out.println("Enter in the artist");
                     artist = sc.next();
-                    original.add(new Song(name, artist));
-                    sort(sortby);
+                    add(new Song(name, artist, time), sortby);
+                    time++;
                     break;
                 case 2:
                     System.out.println(sorted);
@@ -196,7 +236,7 @@ public class Runner {
                     name = sc.next();
                     System.out.println("Enter in the artist");
                     artist = sc.next();
-                    original.remove(new Song(name, artist));
+                    original.remove(new Song(name, artist, 0));
                     sort(sortby);
                     break;
                 case 4:
@@ -214,7 +254,6 @@ public class Runner {
                             i--;
                         }
                     }
-                    sort(sortby);
                     break;
                 case 6:
                     System.out.println("Enter in name");
@@ -225,7 +264,6 @@ public class Runner {
                             i--;
                         }
                     }
-                    sort(sortby);
                     break;
                 case 7:
                     sortby = "artist";
@@ -251,6 +289,47 @@ public class Runner {
                 case 11:
                     System.exit(0);
                     break;
+            }
+        }
+    }
+    public static void add(Song s, String sortby) {
+        if(sortby.equals("name")) {
+            boolean added = false;
+            for(int i = 0;i < original.size();i++) {
+                if(original.get(i).name().compareTo(s.name()) > 0) {
+                    original.add(i, s);
+                    added = true;
+                    break;
+                }
+            }
+            if(!added) {
+                original.add(s);
+            }
+        }
+        else if(sortby.equals("time")) {
+            boolean added = false;
+            for(int i = 0;i < original.size();i++) {
+                if(original.get(i).time() > s.time()) {
+                    original.add(i, s);
+                    added = true;
+                    break;
+                }
+            }
+            if(!added) {
+                original.add(s);
+            }
+        }
+        if(sortby.equals("artist")) {
+            boolean added = false;
+            for(int i = 0;i < original.size();i++) {
+                if(original.get(i).artist().compareTo(s.artist()) > 0) {
+                    original.add(i, s);
+                    added = true;
+                    break;
+                }
+            }
+            if(!added) {
+                original.add(s);
             }
         }
     }
@@ -292,5 +371,7 @@ public class Runner {
         else {
             sorted = original;
         }
+        original = sorted;
     }
-}
+}*/
+
