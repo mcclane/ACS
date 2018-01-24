b9ee595b0e026092c672850a408bcebaca6e3c16
McClane Howland <mcclane.howland@gmail.com>
2018-01-23 20:35:07 -0800

    classwork

--- classwork/0122/Runner.java
+++ classwork/0122/Runner.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
 
+class Task implements Comparable<Task> {
+    String name;
+    int rank;
+    public Task(int rank, String name) {
+        this.name = name;
+        this.rank = rank;
+    }
+    public boolean equals(Object o) {
+        Task t = (Task) o;
+        return toString().equals(t.toString());
+    }
+    public int compareTo(Task t) {
+        return toString().compareTo(t.toString());
+    }
+    public String toString() {
+        return rank+" "+name+"\n";
+    }
+}
 class Node<E> {
     private Node<E> previous;
     private Node<E> next;
@@ -38,9 +56,6 @@ class DLList<E extends Comparable<E>> {
         size = 0;
     }
     private Node<E> getNode(int i) {
-        if(i > 0 && i >= size) {
-            return null;
-        }
         Node<E> current = head.next();
         int counter = 0;
         while(counter < i) {
@@ -95,18 +110,16 @@ class DLList<E extends Comparable<E>> {
         size--;
     }
     public void remove(E data) {
-        if(size == 0) {
-            return;
-        }
-        Node<E> current = head.next();
-        while(current != tail) {
-            if(current.get().equals(data)) {
+        Node<E> tbr = new Node<E>(data);
+        System.out.println(tbr.get());
+        for(Node<E> current = head.next();current != tail;current = current.next()) {
+            if(current.get().equals(tbr.get())) {
+                System.out.println(current.get());
+                Node<E> temp = current.prev();
                 current.prev().setNext(current.next());
-                current.next().setPrev(current.prev());
-                size--;
+                current.next().setPrev(temp);
                 break;
             }
-            current = current.next();
         }
     }
     public void set(int i, E data) {
@@ -122,7 +135,7 @@ class DLList<E extends Comparable<E>> {
         Node<E> current = head.next();
         int time = 1;
         while(current != tail) {
-            out += time+". "+current.get()+"\n ";
+            out += current.get();
             current = current.next();
         }
         return out;
@@ -137,208 +150,88 @@ class DLList<E extends Comparable<E>> {
             System.out.println(n.get());
             _print(n.next());
     }
-/*    public void reverse() {
-        for(Node<E> current = dummy.next();current.get() != null;current = current.prev()) {
-            Node<E> prev = current.prev();
-            current.setPrev(current.next());
-            current.setNext(prev);
-        }
-        Node<E> prev = dummy.prev();
-        dummy.setPrev(dummy.next());
-        dummy.setNext(prev);
-    }
-    */
 }
 public class Runner {
     public static void main(String[] args) {
-        DLList<String> dl = new DLList<String>();
-        dl.add("Bear");
-        dl.add("Bat");
-        dl.add("Dog");
-        dl.add("Horse");
-        dl.add("Rabbit");
-//        dl.add(1, "111");
-//        dl.add(2, "222");
-
-        dl.set(2, "ASDASFSA");
-        dl.print();
+        DLList<Task> tasks = new DLList<Task>();
+        tasks.add(new Task(1, "Drink Water"));
+        tasks.add(new Task(2, "Find Shelter"));
+        tasks.add(new Task(3, "Eat food"));
+        tasks.add(new Task(4, "Make Friends"));
+        tasks.add(new Task(4, "Self Actualization"));
+        System.out.println(tasks);
+        Scanner sc = new Scanner(System.in);
+        int input;
+        int rank;
+        String name;
+        while(true) {
+            System.out.println("1. Display\t\t2. Add a task\t\t3. Remove Task\t\t4. Update Task"); 
+            input = sc.nextInt();
+            if(input == 1) {
+                System.out.println(tasks);
+            }
+            else if(input == 2) {
+                System.out.println("Enter rank");
+                rank = sc.nextInt();
+                System.out.println("Enter name");
+                sc.nextLine();
+                name = sc.nextLine();
+                Task t = new Task(rank, name);
+                boolean added = false;
+                for(int i = 0;i < tasks.size();i++) {
+                    if(tasks.get(i).equals(t)) {
+                        added = true;
+                        break;
+                    }
+                    else if(tasks.get(i).compareTo(t) > 0) {
+                        tasks.add(i+1, t);
+                        added = true;
+                        break;
+                    }
+                }
+                if(!added) {
+                    tasks.add(t);
+                }
+            }
+            else if(input == 3) {
+                System.out.println("Enter rank");
+                rank = sc.nextInt();
+                System.out.println("Enter name");
+                sc.nextLine();
+                name = sc.nextLine().replace("\\n", "");
+                Task t = new Task(rank, name);
+                tasks.remove(t);
+            }
+            else if(input == 4) {
+                System.out.println("Enter rank");
+                rank = sc.nextInt();
+                System.out.println("Enter name");
+                sc.nextLine();
+                name = sc.nextLine();
+                System.out.println("Enter a new rank");
+                int newRank = sc.nextInt();
+                System.out.println("Enter a new name");
+                sc.nextLine();
+                String newName = sc.nextLine();
+                Task tr = new Task(rank, name);
+                Task t = new Task(newRank, newName);
+                tasks.remove(tr);
+                boolean added = false;
+                for(int i = 0;i < tasks.size();i++) {
+                    if(tasks.get(i).equals(t)) {
+                        added = true;
+                        break;
+                    }
+                    else if(tasks.get(i).compareTo(t) > 0) {
+                        tasks.add(i+1, t);
+                        added = true;
+                        break;
+                    }
+                }
+                if(!added) {
+                    tasks.add(t);
+                }
+            }
+        }
     }
 }
-/*public class Runner {
-
-    static int time = 0;
-    static DLList<Song> original = new DLList<Song>();
-    static DLList<Song> sorted = new DLList<Song>();
-
-    public static void main(String[] args) {
-        original.add(new Song("A", "Ant", 0));
-        original.add(new Song("A", "Anteater", 1));
-        original.add(new Song("B", "Bear", 2));
-        original.add(new Song("C", "Cat", 3));
-        original.add(new Song("D", "Duck", 4));
-        time = 5;
-        sorted = original;
-        Scanner sc = new Scanner(System.in);
-        String name;
-        String artist;
-        int index;
-        String sortby = "time";
-        while(true) {
-            System.out.println("1. Add a song\t\t\t2. Display Song List\n3. Delete by name and artist\t4. Delete by index\n5. Delete by artist\t\t6. Delete by name\n7. Sort by artist\t\t8. Sort by name\n9. Sort by time\t\t\t10. Search by artist\n11. Quit");
-            int selection = sc.nextInt();
-            switch(selection) {
-                case 1:
-                    System.out.println("Enter in the name");
-                    name = sc.next();
-                    System.out.println("Enter in the artist");
-                    artist = sc.next();
-                    add(new Song(name, artist, time), sortby);
-                    time++;
-                    break;
-                case 2:
-                    System.out.println(sorted);
-                    break;
-                case 3:
-                    System.out.println("Enter in the name");
-                    name = sc.next();
-                    System.out.println("Enter in the artist");
-                    artist = sc.next();
-                    original.remove(new Song(name, artist, 0));
-                    sort(sortby);
-                    break;
-                case 4:
-                    System.out.println("Enter in an index");
-                    index = sc.nextInt();
-                    original.remove(index);
-                    sort(sortby);
-                    break;
-                case 5:
-                    System.out.println("Enter in artist");
-                    artist = sc.next();
-                    for(int i = 0;i < original.size();i++) {
-                        if(original.get(i).artist().equals(artist)) {
-                            original.remove(i);
-                            i--;
-                        }
-                    }
-                    break;
-                case 6:
-                    System.out.println("Enter in name");
-                    name = sc.next();
-                    for(int i = 0;i < original.size();i++) {
-                        if(original.get(i).name().equals(name)) {
-                            original.remove(i);
-                            i--;
-                        }
-                    }
-                    break;
-                case 7:
-                    sortby = "artist";
-                    sort(sortby);
-                    break;
-                case 8:
-                    sortby = "name";
-                    sort(sortby);
-                    break;
-                case 9:
-                    sortby = "time";
-                    sort(sortby);
-                    break;
-                case 10:
-                    System.out.println("Enter artist");
-                    artist = sc.next();
-                    for(int i = 0;i < original.size();i++) {
-                        if(original.get(i).artist().equals(artist)) {
-                            System.out.println(original.get(i));
-                        }
-                    }
-                    break;
-                case 11:
-                    System.exit(0);
-                    break;
-            }
-        }
-    }
-    public static void add(Song s, String sortby) {
-        if(sortby.equals("name")) {
-            boolean added = false;
-            for(int i = 0;i < original.size();i++) {
-                if(original.get(i).name().compareTo(s.name()) > 0) {
-                    original.add(i, s);
-                    added = true;
-                    break;
-                }
-            }
-            if(!added) {
-                original.add(s);
-            }
-        }
-        else if(sortby.equals("time")) {
-            boolean added = false;
-            for(int i = 0;i < original.size();i++) {
-                if(original.get(i).time() > s.time()) {
-                    original.add(i, s);
-                    added = true;
-                    break;
-                }
-            }
-            if(!added) {
-                original.add(s);
-            }
-        }
-        if(sortby.equals("artist")) {
-            boolean added = false;
-            for(int i = 0;i < original.size();i++) {
-                if(original.get(i).artist().compareTo(s.artist()) > 0) {
-                    original.add(i, s);
-                    added = true;
-                    break;
-                }
-            }
-            if(!added) {
-                original.add(s);
-            }
-        }
-    }
-    public static void sort(String sortby) {
-        if(sortby.equals("name")) {
-            sorted = new DLList<Song>();
-            for(int i = 0;i < original.size();i++) {
-                boolean added = false;
-                for(int j = 0;j < sorted.size();j++) {
-                    if(sorted.get(j).name().compareTo(original.get(i).name()) > 0) {
-                        System.out.println(j);
-                        sorted.add(j, original.get(i));
-                        added = true;
-                        break;
-                    }
-                }
-                if(!added) {
-                    sorted.add(original.get(i));
-                }
-            }
-        }
-        else if(sortby.equals("artist")) {
-            sorted = new DLList<Song>();
-            for(int i = 0;i < original.size();i++) {
-                boolean added = false;
-                for(int j = 0;j < sorted.size();j++) {
-                    if(sorted.get(j).artist().compareTo(original.get(i).artist()) > 0) {
-                        System.out.println(j);
-                        sorted.add(j, original.get(i));
-                        added = true;
-                        break;
-                    }
-                }
-                if(!added) {
-                    sorted.add(original.get(i));
-                }
-            }
-        }
-        else {
-            sorted = original;
-        }
-        original = sorted;
-    }
-}*/
-
